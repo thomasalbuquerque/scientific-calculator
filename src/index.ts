f0133957dbd6b6d0c21a49a4484450fd27a09414
Thomas Albuquerque <tealbthomas@gmail.com>
2024-02-08 21:27:39 -0300

    feat: add new calculations

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,41 @@ function main() {
 
   const sumNetWithTax2 = add([new Decimal('13.05227896647379'), divastro9_12]);
   console.log('sumNetWithTax2', sumNetWithTax2);
+
+  const sumT1 = add([new Decimal('4645.204342122197'), new Decimal('7.795657877803235')]);
+  console.log('sumT1', sumT1);
+
+  const sumT2 = add([
+    new Decimal('2568.405220540000000000'),
+    new Decimal('48.67217106109838'),
+    new Decimal('50.18217106109838'),
+    new Decimal('49.39217106109838'),
+  ]);
+  console.log('sumT2', sumT2);
+
+  const sumT3 = add([
+    new Decimal('3371.756500000000000000'),
+    new Decimal('56.052171061098385'),
+    new Decimal('79.70217106109838'),
+    new Decimal('250.6821710610984'),
+  ]);
+  console.log('sumT3', sumT3);
+
+  const sumT4 = add([
+    new Decimal('0.006192047225220400'),
+    new Decimal('48.67217106109838'),
+    new Decimal('50.18217106109838'),
+    new Decimal('49.39217106109838'),
+  ]);
+  console.log('sumT4', sumT4);
+
+  const sumT5 = add([
+    new Decimal('0.003276879908947176'),
+    new Decimal('56.052171061098385'),
+    new Decimal('79.70217106109838'),
+    new Decimal('250.6821710610984'),
+  ]);
+  console.log('sumT5', sumT5);
 }
 
 main();
